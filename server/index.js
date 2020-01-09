e735752d54a895b97cca2198a32cf9eb6dcf7cda
naoki <duvingtdeux@gmail.com>
2020-01-09 15:13:47 +0900

    installed mongoose and established connection to mongodb

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
+// Mongo
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/ssr1', {useNewUrlParser: true})
+
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
@@ -23,6 +27,7 @@ async function start () {
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
+
 
   // Listen the server
   app.listen(port, host)
