cf0cc585af328f0215244dc2c87e2934932326ac
naoki <duvingtdeux@gmail.com>
2020-01-09 14:16:36 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: {
+    message: 'hello ssr1'
+  },
+  components: {
+  }
+}
+</script>
+
